7921bd882df8317ca1f38aba06c20d0498f75ad0
ikohunter <lorikohendra@gmail.com>
2025-11-21 17:37:38 +0700

    Menambahkan file konfigurasi ke main

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "logistic_corner";
+
+
+$site_name = "Logistic Corner";
+$site_url = "http://localhost/logistic_corner";
+
+
+$session_timeout = 3600;
+
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
